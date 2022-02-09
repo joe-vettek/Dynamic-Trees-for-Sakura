b0bb4a879297fbf20ed62295e2d9a723726c7def
joe-vettek <my1233215@126.com>
2022-02-09 22:33:05 +0800

    crash mix

--- src/main/java/xueluoanping/dtsakuracompact/block/BlockMapleSpile_extened.java
+++ src/main/java/xueluoanping/dtsakuracompact/block/BlockMapleSpile_extened.java
@@ -3,19 +3,17 @@ package xueluoanping.dtsakuracompact.block;
 import cn.mcmod.sakura.SakuraMain;
 import cn.mcmod.sakura.block.BlockLoader;
 import cn.mcmod.sakura.block.BlockMapleSpile;
-import cn.mcmod.sakura.block.BlockMapleSyrupCauldron;
 import cn.mcmod.sakura.block.tree.BlockMapleSapLog;
 import cn.mcmod.sakura.client.SakuraParticleType;
 import cn.mcmod.sakura.tileentity.TileEntityMapleCauldron;
-import cn.mcmod_mmf.mmlib.block.BlockFacing;
 import com.ferreusveritas.dynamictrees.blocks.BlockBranch;
+import com.ferreusveritas.dynamictrees.entities.EntityFallingTree;
+import com.ferreusveritas.dynamictrees.util.BranchDestructionData;
 import net.minecraft.block.Block;
-import net.minecraft.block.material.Material;
-import net.minecraft.block.properties.PropertyInteger;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
-import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
@@ -26,6 +24,7 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 import xueluoanping.dtsakuracompact.DynamicTrees_Sakura;
 import xueluoanping.dtsakuracompact.pro.info;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 import static net.minecraft.init.Blocks.AIR;
@@ -64,8 +63,7 @@ public class BlockMapleSpile_extened extends BlockMapleSpile {
      */
     public boolean canPlaceBlockAt(World worldIn, BlockPos pos) {
         for (EnumFacing enumfacing : FACING.getAllowedValues())
-            if (this.canPlaceAt(worldIn, pos, enumfacing))
-                return true;
+            if (this.canPlaceAt(worldIn, pos, enumfacing)) return true;
         return false;
     }
 
@@ -88,9 +86,10 @@ public class BlockMapleSpile_extened extends BlockMapleSpile {
         if (facing != EnumFacing.UP && facing != EnumFacing.DOWN) {
             boolean flag1 = !isExceptBlockForAttachWithPiston(block);
             boolean flag2 = block instanceof BlockMapleSapLog && iblockstate.getValue(BlockMapleSapLog.SAP_AGE).intValue() < 5;
-            if (block == Block.getBlockFromName(info.modid + ":maplebranch")) flag2 = true;
-            if (block != AIR)
-                DynamicTrees_Sakura.LOGGER.debug("DTS:" + block.getLocalizedName() + iblockstate.getProperties().toString());
+            if (block == Block.getBlockFromName(info.modid + ":maplebranch"))
+                flag2 = true;
+//            if (block != AIR)
+//                DynamicTrees_Sakura.LOGGER.debug("DTS:" + block.getLocalizedName() + iblockstate.getProperties().toString());
             return flag1 && flag2;
         }
         return false;
@@ -102,24 +101,52 @@ public class BlockMapleSpile_extened extends BlockMapleSpile {
             EnumFacing facing = state.getValue(FACING);
             BlockPos blockpos = pos.offset(facing.getOpposite());
             BlockPos blockpos0 = pos.down();
-            IBlockState iblockstate = worldIn.getBlockState(blockpos);
-            IBlockState iblockstate0 = worldIn.getBlockState(blockpos0);
+            IBlockState logState = worldIn.getBlockState(blockpos);
+            IBlockState mapleCauldronState = worldIn.getBlockState(blockpos0);
             try {
                 TileEntity te = worldIn.getTileEntity(blockpos0);
                 if (te instanceof TileEntityMapleCauldron)
-                    ((TileEntityMapleCauldron) te)
-                            .getTank().fill(new FluidStack(BlockLoader.MAPLE_SYRUP_FLUID, 500), true);
+                    ((TileEntityMapleCauldron) te).getTank().fill(new FluidStack(BlockLoader.MAPLE_SYRUP_FLUID, 500), true);
+                else return;
             } catch (Exception e) {
-                DynamicTrees_Sakura.LOGGER.debug("DTS+randomTick" + iblockstate0.toString());
+                DynamicTrees_Sakura.LOGGER.debug("DTS+randomTick" + mapleCauldronState.toString());
             }
-            if (worldIn.rand.nextInt(5000) == 0)
+            //sakura-maplelog
+            if (worldIn.rand.nextInt(5000) == 0 && logState.getBlock() instanceof BlockMapleSapLog) {
                 try {
-                    worldIn.setBlockState(blockpos, iblockstate.withProperty(BlockMapleSapLog.SAP_AGE, iblockstate.getValue(BlockMapleSapLog.SAP_AGE).intValue() + 1));
-                } catch (Exception e) {
-                    worldIn.setBlockState(blockpos, iblockstate.withProperty(PropertyInteger.create("radius", 0, 8), iblockstate.getValue(PropertyInteger.create("radius", 0, 8)).intValue() - 1));
-
+                    if(logState.getValue(BlockMapleSapLog.SAP_AGE) < 5)
+                    worldIn.setBlockState(blockpos, logState.withProperty(BlockMapleSapLog.SAP_AGE, logState.getValue(BlockMapleSapLog.SAP_AGE).intValue() + 1));
+                } catch (Exception e0) {
                 }
+            }
+            //dtsakura-maplelog
+            if (worldIn.rand.nextInt(3000) == 0 && logState.getBlock() instanceof BlockBranch) {
+                rotBranch(logState, worldIn, blockpos.up(), EnumFacing.UP);
+                rotBranch(logState, worldIn, blockpos, EnumFacing.UP);
+                rotBranch(logState, worldIn, blockpos.down(), EnumFacing.UP);
+            }
+
         }
+    }
+
+    private boolean rotBranch(IBlockState logState, World worldIn, BlockPos blockpos, EnumFacing value) {
+        try {
+            if (((BlockBranch) logState.getBlock()).getRadius(logState) == 1 && logState.getBlock() instanceof BlockBranch) {
+                {
+//                            ((BlockBranch) logState.getBlock()).destroyBranchFromNode(worldIn, blockpos, state.getValue(FACING), true);
+                    BranchDestructionData destroyData = ((BlockBranch) logState.getBlock()).destroyBranchFromNode(worldIn, blockpos, value, false);
+                    if (destroyData.getNumBranches() != 0)
+                        EntityFallingTree.dropTree(worldIn, destroyData, new ArrayList<ItemStack>(0), EntityFallingTree.DestroyType.HARVEST);
+                    return true;
+                }
+            } else if (logState.getBlock() instanceof BlockBranch) {
+                ((BlockBranch) logState.getBlock()).setRadius(worldIn, blockpos, ((BlockBranch) logState.getBlock()).getRadius(logState) - 1, value);
+            }
+        } catch (Exception exception2) {
+//            DynamicTrees_Sakura.LOGGER.debug("DTS+randomTick" + ((BlockBranch) logState.getBlock()).getRadius(logState));
+//            exception2.printStackTrace();
+        }
+        return false;
     }
 
     public static boolean canWork(World worldIn, BlockPos pos, IBlockState state) {
@@ -127,8 +154,8 @@ public class BlockMapleSpile_extened extends BlockMapleSpile {
         BlockPos blockpos = pos.offset(facing.getOpposite());
         IBlockState iblockstate = worldIn.getBlockState(blockpos);
         Block block = iblockstate.getBlock();
-        if ((block instanceof BlockMapleSapLog && iblockstate.getValue(BlockMapleSapLog.SAP_AGE).intValue() < 5)
-        ) return true;
+        if ((block instanceof BlockMapleSapLog && iblockstate.getValue(BlockMapleSapLog.SAP_AGE).intValue() < 5))
+            return true;
         if (block == Block.getBlockFromName(info.modid + ":maplebranch")) return true;
         return false;
     }
--- src/main/java/xueluoanping/dtsakuracompact/block/LeavesPaging.java
+++ src/main/java/xueluoanping/dtsakuracompact/block/LeavesPaging.java
@@ -11,7 +11,11 @@ import com.ferreusveritas.dynamictrees.blocks.LeavesStateMapper;
 import com.ferreusveritas.dynamictrees.util.JsonHelper;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import net.minecraft.block.Block;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
@@ -33,70 +37,80 @@ import java.util.logging.Logger;
 //Changed the randomTickDisplay
 public class LeavesPaging extends com.ferreusveritas.dynamictrees.blocks.LeavesPaging {
 
-	///////////////////////////////////////////
-	//BLOCK PAGING
-	///////////////////////////////////////////
+    ///////////////////////////////////////////
+    //BLOCK PAGING
+    ///////////////////////////////////////////
 
-	private static HashMap<String, HashMap<Integer, BlockDynamicLeaves>> modLeavesArray = new HashMap<>();
-	private static HashMap<String, Integer> modLastSeq = new HashMap<>();
+    private static HashMap<String, HashMap<Integer, BlockDynamicLeaves>> modLeavesArray = new HashMap<>();
+    private static HashMap<String, Integer> modLastSeq = new HashMap<>();
 
-	private static String autoModId(@Nullable String modid) {
-		if (modid == null || "".equals(modid)) {
-			ModContainer mc = Loader.instance().activeModContainer();
-			modid = mc == null ? ModConstants.MODID : mc.getModId();
-		}
-		return modid;
-	}
-	public static BlockDynamicLeaves getLeavesBlockForSequence(@Nullable String modid, int seq, @Nonnull ILeavesProperties leavesProperties) {
-		BlockDynamicLeaves leaves = getLeavesBlockForSequence(modid, seq);
-		int tree = seq & 3;
-		leavesProperties.setDynamicLeavesState(leaves.getDefaultState().withProperty(BlockDynamicLeaves.TREE, tree));
-		leaves.setProperties(tree, leavesProperties);
-		return leaves;
-	}
-	/**
-	 * A convenience function for packing 4 {@link BlockDynamicLeaves} blocks into one Minecraft block using metadata.
-	 * Changed the randomTickDisplay
-	 *
-	 * @param modid
-	 * @param seq
-	 * @return
-	 */
-	private static BlockDynamicLeaves getLeavesBlockForSequence(@Nullable String modid, int seq) {
-		int key = seq / 4;
-		String regname = "leaves" + key;
+    private static String autoModId(@Nullable String modid) {
+        if (modid == null || "".equals(modid)) {
+            ModContainer mc = Loader.instance().activeModContainer();
+            modid = mc == null ? ModConstants.MODID : mc.getModId();
+        }
+        return modid;
+    }
 
-		return getLeavesMapForModId(modid).computeIfAbsent(key, k -> (BlockDynamicLeaves) new BlockDynamicLeaves(){
-			@Override
-			public void randomDisplayTick(IBlockState stateIn, World worldIn, BlockPos pos, Random rand) {
-				if(rand.nextInt(40)<1){int j = rand.nextInt(2) * 2 - 1;
-					int k = rand.nextInt(2) * 2 - 1;
+    public static BlockDynamicLeaves getLeavesBlockForSequence(@Nullable String modid, int seq, @Nonnull ILeavesProperties leavesProperties) {
+        BlockDynamicLeaves leaves = getLeavesBlockForSequence(modid, seq);
+        int tree = seq & 3;
+        leavesProperties.setDynamicLeavesState(leaves.getDefaultState().withProperty(BlockDynamicLeaves.TREE, tree));
+        leaves.setProperties(tree, leavesProperties);
+        return leaves;
+    }
 
-					double d0 = pos.getX() + 0.5D + 0.25D * j;
-					double d1 = pos.getY() - 0.15D;
-					double d2 = pos.getZ() + 0.5D + 0.25D * k;
-					double d3 = rand.nextFloat() * j * 0.1D;
-					double d4 = ((rand.nextFloat()) * 0.055D) + 0.015D;
-					double d5 = rand.nextFloat() * k * 0.1D;
-					if (stateIn.getBlock().getRegistryName().toString().equals("dtsakuracompact:leaves0"))
-						SakuraMain.proxy.spawnParticle(SakuraParticleType.LEAVESSAKURA, d0, d1, d2, d3, -d4, d5);
-					if (stateIn.getBlock().getRegistryName().toString().equals("dtsakuracompact:leaves1")
-							&& stateIn.getBlock().getMetaFromState(stateIn) / 4 == 0)
-						SakuraMain.proxy.spawnParticle(SakuraParticleType.MAPLEGREEN, d0, d1, d2, d3, -d4, d5);
-					if (stateIn.getBlock().getRegistryName().toString().equals("dtsakuracompact:leaves1")
-							&& stateIn.getBlock().getMetaFromState(stateIn) / 4 == 1)
-						SakuraMain.proxy.spawnParticle(SakuraParticleType.MAPLEORANGE, d0, d1, d2, d3, -d4, d5);
-					if (stateIn.getBlock().getRegistryName().toString().equals("dtsakuracompact:leaves1")
-							&& stateIn.getBlock().getMetaFromState(stateIn) / 4 == 2)
-						SakuraMain.proxy.spawnParticle(SakuraParticleType.MAPLERED, d0, d1, d2, d3, -d4, d5);
-					if (stateIn.getBlock().getRegistryName().toString().equals("dtsakuracompact:leaves1")
-							&& stateIn.getBlock().getMetaFromState(stateIn) / 4 == 3)
-						SakuraMain.proxy.spawnParticle(SakuraParticleType.MAPLEYELLOW, d0, d1, d2, d3, -d4, d5);}
-				super.randomDisplayTick(stateIn, worldIn, pos, rand);
-			}
-		}.setDefaultNaming(autoModId(modid), regname));
-	}
+    /**
+     * A convenience function for packing 4 {@link BlockDynamicLeaves} blocks into one Minecraft block using metadata.
+     * Changed the randomTickDisplay
+     *
+     * @param modid
+     * @param seq
+     * @return
+     */
+    private static BlockDynamicLeaves getLeavesBlockForSequence(@Nullable String modid, int seq) {
+        int key = seq / 4;
+        String regname = "leaves" + key;
 
+        return getLeavesMapForModId(modid).computeIfAbsent(key, k -> (BlockDynamicLeaves) new BlockDynamicLeaves() {
+
+
+            @Override
+            public void randomDisplayTick(IBlockState stateIn, World worldIn, BlockPos pos, Random rand) {
+                if (stateIn.getBlock().getRegistryName().toString().equals("dtsakuracompact:leaves0")) {
+                    this.setLightOpacity(0);
+                    this.setLightLevel(0.75F);
+                }
+                if (rand.nextInt(40) < 1) {
+                    int j = rand.nextInt(2) * 2 - 1;
+                    int k = rand.nextInt(2) * 2 - 1;
+
+                    double d0 = pos.getX() + 0.5D + 0.25D * j;
+                    double d1 = pos.getY() - 0.15D;
+                    double d2 = pos.getZ() + 0.5D + 0.25D * k;
+                    double d3 = rand.nextFloat() * j * 0.1D;
+                    double d4 = ((rand.nextFloat()) * 0.055D) + 0.015D;
+                    double d5 = rand.nextFloat() * k * 0.1D;
+                    if (stateIn.getBlock().getRegistryName().toString().equals("dtsakuracompact:leaves0"))
+                        SakuraMain.proxy.spawnParticle(SakuraParticleType.LEAVESSAKURA, d0, d1, d2, d3, -d4, d5);
+                    if (stateIn.getBlock().getRegistryName().toString().equals("dtsakuracompact:leaves1")
+                            && stateIn.getBlock().getMetaFromState(stateIn) / 4 == 0)
+                        SakuraMain.proxy.spawnParticle(SakuraParticleType.MAPLEGREEN, d0, d1, d2, d3, -d4, d5);
+                    if (stateIn.getBlock().getRegistryName().toString().equals("dtsakuracompact:leaves1")
+                            && stateIn.getBlock().getMetaFromState(stateIn) / 4 == 1)
+                        SakuraMain.proxy.spawnParticle(SakuraParticleType.MAPLEORANGE, d0, d1, d2, d3, -d4, d5);
+                    if (stateIn.getBlock().getRegistryName().toString().equals("dtsakuracompact:leaves1")
+                            && stateIn.getBlock().getMetaFromState(stateIn) / 4 == 2)
+                        SakuraMain.proxy.spawnParticle(SakuraParticleType.MAPLERED, d0, d1, d2, d3, -d4, d5);
+                    if (stateIn.getBlock().getRegistryName().toString().equals("dtsakuracompact:leaves1")
+                            && stateIn.getBlock().getMetaFromState(stateIn) / 4 == 3)
+                        SakuraMain.proxy.spawnParticle(SakuraParticleType.MAPLEYELLOW, d0, d1, d2, d3, -d4, d5);
+                }
+                super.randomDisplayTick(stateIn, worldIn, pos, rand);
+            }
+
+        }.setDefaultNaming(autoModId(modid), regname));
+    }
 
 
 }
